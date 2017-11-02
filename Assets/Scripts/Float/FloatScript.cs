90d121aec3214d03100fb9e6dc03ab14a138e504
Shamainco <ramirobelluardo1993@gmail.com>
2017-11-02 01:25:09 -0300

    Arreglado Efecto Plataformas

--- Assets/Scripts/Float/FloatScript.cs
+++ Assets/Scripts/Float/FloatScript.cs
@@ -4,21 +4,42 @@ using UnityEngine;
 
 public class FloatScript : MonoBehaviour {
 
+	public GameObject anterior;
+	public GameObject posterior;
+
 	private void OnTriggerEnter2D(Collider2D col)
-	{if (col.gameObject.tag == "Player")
-		{ 
-			transform.position=new Vector3(transform.position.x, transform.position.y+0.05f, transform.position.z);
+	{
+		if (col.gameObject.tag == "Player") { 
+			transform.position = new Vector3 (transform.position.x, transform.position.y + 0.05f, transform.position.z);
 			StartCoroutine ("tiempo");
 
-			} 
-	
+			if (anterior != null) {
+				anterior.transform.position = new Vector3 (anterior.transform.position.x, anterior.transform.position.y + 0.025f, anterior.transform.position.z);
+				StartCoroutine ("tiempoAnterior");
+			}
+			if (posterior != null) {
+				posterior.transform.position = new Vector3 (posterior.transform.position.x, posterior.transform.position.y + 0.025f, posterior.transform.position.z);
+				StartCoroutine ("tiempoPosterior");
+			}
+
+		} 
+	}
 
 
-}
 
 	IEnumerator tiempo (){
-		yield return new WaitForSeconds (0.5f);
+		yield return new WaitForSeconds (0.3f);
 		transform.position=new Vector3(transform.position.x, transform.position.y-0.05f, transform.position.z);
+
+	}
+	IEnumerator tiempoAnterior (){
+		yield return new WaitForSeconds (0.3f);
+		anterior.transform.position=new Vector3(anterior.transform.position.x, anterior.transform.position.y-0.025f, anterior.transform.position.z);
+
+	}
+	IEnumerator tiempoPosterior (){
+		yield return new WaitForSeconds (0.3f);
+		posterior.transform.position=new Vector3(posterior.transform.position.x, posterior.transform.position.y-0.025f, posterior.transform.position.z);
 
 	}
 }
